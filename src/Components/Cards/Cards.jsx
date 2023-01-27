b7e6a7d925b9d3e8171d73fca6548d723884e2cf
rabbani4u <rabbani.iu31@gmail.com>
2023-01-28 00:03:25 +0600

    update

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,36 +1,40 @@
 import React from "react";
-import CardItem from "./CardItem";
-import "./Cards.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Cards() {
   return (
-    <div className="card">
-      <div className="card__row">
-        <CardItem
-          img="https://images.unsplash.com/photo-1674117171406-61b102bfbfb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-          title="Rabbani Khan"
-          tel="+8801822446673"
-        />
-        <CardItem
-          img="https://images.unsplash.com/photo-1674117171406-61b102bfbfb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-          title="Rabbani Khan"
-          tel="+8801822446673"
-        />
-        <CardItem
-          img="https://images.unsplash.com/photo-1674117171406-61b102bfbfb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-          title="Rabbani Khan"
-          tel="+8801822446673"
-        />
-        <CardItem
-          img="https://images.unsplash.com/photo-1674117171406-61b102bfbfb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-          title="Rabbani Khan"
-          tel="+8801822446673"
-        />
-        <CardItem
-          img="https://images.unsplash.com/photo-1674117171406-61b102bfbfb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDIwfEZ6bzN6dU9ITjZ3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
-          title="Rabbani Khan"
-          tel="+8801822446673"
-        />
+    <div className="container my-20">
+      <div class="row no-gutters">
+        <div class="col-sm">
+          <img
+            src="https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80"
+            alt=""
+          />
+        </div>
+        <div class="col-sm">
+          <img
+            src="https://images.unsplash.com/photo-1523805009345-7448845a9e53?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+            alt=""
+          />
+        </div>
+        <div class="col-sm">
+          <img
+            src="https://images.unsplash.com/photo-1661866816132-f82fcaa22401?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt=""
+          />
+        </div>
+        <div class="col-sm">
+          <img
+            src="https://images.unsplash.com/photo-1662041903631-cd7732fe5e5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt=""
+          />
+        </div>
+        <div class="col-sm">
+          <img
+            src="https://images.unsplash.com/photo-1459183885421-5cc683b8dbba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
